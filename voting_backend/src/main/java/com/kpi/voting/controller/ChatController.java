333ae1efb2cd8eaf91fa9d34541e0c241aa872da
Roman Harmash <rondo104@gmail.com>
2019-04-09 01:24:58 +0300

    refactoring-5: Extend REST API

--- voting_backend/src/main/java/com/kpi/voting/controller/ChatController.java
+++ voting_backend/src/main/java/com/kpi/voting/controller/ChatController.java
@@ -4,6 +4,8 @@ import com.kpi.voting.domain.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 /**
  * @author Roman.Harmash
  * @version 1.0
@@ -15,6 +17,11 @@ public class ChatController {
 
     @Autowired
     private ChatService chatService;
+
+    @GetMapping()
+    public @ResponseBody List<String> getMessages() {
+        return chatService.getAllMessages();
+    }
 
     @PutMapping()
     public void sendMessage(@RequestBody String message) {
--- voting_backend/src/main/java/com/kpi/voting/controller/QuestionController.java
+++ voting_backend/src/main/java/com/kpi/voting/controller/QuestionController.java
@@ -5,6 +5,8 @@ import com.kpi.voting.domain.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Objects;
+
 @RestController
 @RequestMapping("question")
 public class QuestionController {
@@ -16,6 +18,17 @@ public class QuestionController {
     public @ResponseBody
     Question getLastQuestion() {
         return questionService.getLastQuestion();
+    }
+
+    @PostMapping()
+    public @ResponseBody
+    String createQuestion(@RequestBody String question) {
+        final Long id = questionService.createQuestion(question);
+        if (Objects.isNull(id)) {
+            return "Something went wrong. Can't create question " + question;
+        } else {
+            return "Created question [" + question + "] with id = " + id;
+        }
     }
 
 }

a18509725f158f87186a95a14f9a5b5077fa858d
hitrch <vladislawpaliy@gmail.com>
2019-05-09 14:10:06 +0300

    raw frontend

--- delivery_frontend/src/app/order/order.component.ts
+++ delivery_frontend/src/app/order/order.component.ts
@@ -1,18 +1,28 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-/*import {HttpClient} from "@angular/common/http";*/
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ToasterService} from 'angular2-toaster';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.scss']
 })
-export class OrderComponent implements OnInit, OnDestroy {
-  constructor(/*private httpClient: HttpClient*/) {}
 
-  ngOnInit(): void {
+export class OrderComponent {
 
+  constructor(private httpClient: HttpClient,
+              private toaster: ToasterService) {
   }
 
-  ngOnDestroy(): void {
+  sendOrder(destination: string, goods: string, price: string): void {
+    this.httpClient.put('order', destination);
+    this.httpClient.put('order', goods);
+    if (price === ' ') {
+      this.httpClient.put('order', Double.parseDouble(price))
+        .subscribe(() => this.toaster.pop('success', 'Order proceeded.'));
+    } else {
+      this.httpClient.put()
+        .subscribe(() => this.toaster.pop('success', 'Order proceeded.'));
+    }
   }
 }
